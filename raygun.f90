022284e606922da4c2d03d7fedf54109858cc705
cosmonaut <guavajellyfish@yahoo.com>
2009-08-22 23:04:21 -0600

    Set up 4 plot window

--- raygun.f90
+++ raygun.f90
@@ -1,21 +1,21 @@
-
 program raygun
 
   use plplot, PI => PL_PI
   use xml_data_config
+
   implicit none
 
-  integer                            :: n
+  integer                            :: argc
   character(len=100)                 :: file
-  double precision                   :: i
-  double precision, dimension(300)   :: rays = 0
+  double precision                   :: test
+  double precision, dimension(3000)  :: rays = 0
 
-  i = 1.34567834563987498374
-  print *, i
+  test = 1.34567834563987498374
+  print *, test
 
-  n = IARGC()
+  argc = IARGC()
 
-  if(n > 1) then
+  if(argc > 1) then
      print *, "ERROR, too many arguments"
      stop
   endif
@@ -30,26 +30,29 @@ program raygun
      call read_xml_file_config("test.xml")
   endif
 
+  
+
   print *, rays(1)
   call plot_that_shit()
   
-  print *, hibound
-
   !call xml_process( 'test.xml', attribs, data, startfunc, datafunc, endfunc, 0, error )
   
-!  print *,"O HAI WORLDS!"
+  !print *,"O HAI WORLDS!"
   
 contains
 
   subroutine plot_that_shit()
     real(plflt), dimension(1:2) :: x, y
-    real(plflt)                 :: xmin, xmax, ymin, ymax, rot
+    real(plflt)                 :: xmin, xmax, ymin, ymax, zmin, zmax, rot
     integer                     :: just, axis
 
     xmin = lobound(1)
     ymin = lobound(2)
     xmax = hibound(1)
     ymax = hibound(2)
+    zmin = lobound(3)
+    zmax = hibound(3)
+
     just = 1
     axis = 0
     
@@ -59,7 +62,9 @@ contains
     y(2) = 5
 
     rot = 1
-
+    
+    !We will always use Z as the optical axis.
+    
     !change color index 15 to be black
     call plscol0(15, 0, 0, 0)
     
@@ -68,25 +73,38 @@ contains
     call plsfnam("wat.pdf")
     call plsdev("pdfcairo")
 
+    call plssub(2, 2)
 
-    !set background, initialize.
+    !set background, initialize, font crap.
     call plscolbg(255, 255, 255)
     call plinit()
     call plfont(2)
-    !call plsfont(1, 0, 0)
-    call plfontld(1)
+    !call plfontld(1)
 
     call plcol0(15)
     call plenv(xmin, xmax, ymin, ymax, just, axis)
+    call pllab("X Axis", "Y Axis", "View from Z axis. #[0x212b]")
 
-    call pllab("Blah #gx", "Meow", "#[0x00c5]#[0x212b]#[0x03c8]#[0x01fa]#[0x00b0]")
     call plcol0(1)
     call plline(x, y)
 
+    call plcol0(15)
+    call plenv(zmin, zmax, ymin, ymax, just, axis)
+    call pllab("Z Axis", "Y Axis", "View from X axis. #[0x212b]")
+
+    call plcol0(15)
+    call plenv(zmin, zmax, xmin, xmax, just, axis)
+    call pllab("Z Axis", "X Axis", "View from Y axis. #[0x212b]")
+
+    call plcol0(15)
+    call plenv(zmin, zmax, xmin, xmax, just, axis)
+    call pllab("X Axis", "Y Axis", "View from detector. #[0x212b]")
+
+
     call plend()
-    
+
   end subroutine plot_that_shit
 
-  
+!kthxbai  
 end program raygun
 
